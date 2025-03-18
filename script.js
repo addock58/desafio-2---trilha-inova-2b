d57ed54afeb15994a4754e3e2a6ce33ab62a1578
addock58 <pedromonteiro0508@gmail.com>
2025-03-18 18:03:29 -0300

    Correção de codigo, adições CSS, adição sistema de verificação de campo JS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let radioButtons = document.querySelectorAll('input[type="radio"]');
+let formulario = document.getElementById('cadastro');
 
+// Aréa responsável por alterar o estilo das trilhas quando forem clicadas
 radioButtons.forEach((radio) => {
   radio.addEventListener("change", () => {
     // Restaura o estilo de todos os labels
@@ -18,13 +20,54 @@ radioButtons.forEach((radio) => {
   });
 });
 
+// Área responsável por validar se os campos do formulário estão preenchidos
+formulario.addEventListener('submit', (event) => {
+  // Impede o envio do formulário
+  event.preventDefault();
 
-// function requiredOff(){
-//   const inputs = document.querySelectorAll("#cadastro input")
+  // Seleciona todos os campos do formulário
+  const campos = document.querySelectorAll(".validate");
+  console.log(campos);
+
+  let n = 0
   
-//   inputs.forEach((input) => {
-//     input.removeAttribute("required");
-//   });
+  for(let i = 0; i < campos.length; i++){
+    
+    if(!campos[i].value){ //se o campo não estiver preenchido
+      mensagemDeErro(campos[i].id);
 
-//   console.log("Atributo required removido")
-// };
+      // Adiciona o foco no primeiro campo que não foi preenchido
+      n == 0 ? campos[i].focus(): null; 
+      n++
+    } 
+    else {
+      //limpar mensagem de erro do que já foi preenchido depois da primeira tentativa
+      document.getElementById(campos[i].id).classList.remove('error');
+      document.querySelector(`p[for="${campos[i].id}"]`).style.display = 'none';
+    };
+  };
+
+});
+
+// Adiciona a classe error ao campo e adivando a mensagem de erro
+function mensagemDeErro(value) {
+  let input = document.getElementById(value).type;
+  // Verifica se o campo é um input do tipo file
+  if (input != "file") {
+    document.getElementById(value).classList.add('error');
+    document.querySelector(`p[for="${value}"]`).style.display = 'flex';
+  } else {
+    document.querySelector(`p[for="${value}"]`).style.display = 'flex';
+  }
+}
+
+// Função para resetar a mensagem de erro
+function resetMensagemDeErro(value) {
+  document.getElementById(value).classList.remove('error');
+}
+
+// Adiciona event listeners para resetar a mensagem de erro ao clicar nos inputs
+const campos = document.querySelectorAll(".validate");
+campos.forEach((campo) => {
+  campo.addEventListener('focus', () => resetMensagemDeErro(campo.id));
+});
